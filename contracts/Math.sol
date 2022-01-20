146a7b099268ebe903ef72c6ab78426963f2eb03
abdolazim35@yahoo.com <abdolazim35@yahoo.com>
2022-01-20 12:59:07 +0330

    100% cov

--- contracts/Math.sol
+++ contracts/Math.sol
@@ -5,8 +5,4 @@ contract Math {
     function add(uint256 _x, uint256 _y) public pure returns (uint256) {
         return _x + _y;
     }
-
-     function sub(uint256 _x, uint256 _y) public pure returns (uint256) {
-        return _x + _y;
-    }
 }
