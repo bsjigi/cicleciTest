f43bcab0f79fb9de34b1a33e717e88260310218e
abdolazim35@yahoo.com <abdolazim35@yahoo.com>
2022-01-20 10:39:10 +0330

    add config

--- contracts/Math.sol
+++ contracts/Math.sol
@@ -5,4 +5,8 @@ contract Math {
     function add(uint256 _x, uint256 _y) public pure returns (uint256) {
         return _x + _y;
     }
+
+     function sub(uint256 _x, uint256 _y) public pure returns (uint256) {
+        return _x + _y;
+    }
 }
