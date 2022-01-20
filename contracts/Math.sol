e0be9902976577ac697ff5892765965460785a53
abdolazim35@yahoo.com <abdolazim35@yahoo.com>
2022-01-20 15:30:11 +0330

    fix readme

--- contracts/Math.sol
+++ contracts/Math.sol
@@ -9,4 +9,16 @@ contract Math {
     function sub(uint256 _x, uint256 _y) public pure returns (uint256) {
         return _x - _y;
     }
+
+    function sub1(uint256 _x, uint256 _y) public pure returns (uint256) {
+        return _x - _y;
+    }
+
+    function sub2(uint256 _x, uint256 _y) public pure returns (uint256) {
+        return _x - _y;
+    }
+
+    function sub3(uint256 _x, uint256 _y) public pure returns (uint256) {
+        return _x - _y;
+    }
 }
