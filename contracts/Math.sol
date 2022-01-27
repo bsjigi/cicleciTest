324288a29db4765fc4657225195631b899238347
abdolazim35@yahoo.com <abdolazim35@yahoo.com>
2022-01-27 17:44:09 +0330

    complete branch

--- contracts/Math.sol
+++ contracts/Math.sol
@@ -13,8 +13,4 @@ contract Math {
     function mul(uint256 _x, uint256 _y) public pure returns (uint256) {
         return _x * _y;
     }
-
-    function div(uint256 _x, uint256 _y) public pure returns (uint256) {
-        return _x / _y;
-    }
 }
